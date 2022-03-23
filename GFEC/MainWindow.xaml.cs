568a028d43807ac514a2acdf22524edc8d390331
Vasilis Merevis <vasilis.merevis@outlook.com>
2022-03-23 02:03:21 +0200

    Minor changes

--- GFEC/MainWindow.xaml.cs
+++ GFEC/MainWindow.xaml.cs
@@ -38,7 +38,38 @@ namespace GFEC
         public SeriesCollection Something { get; set; }
         public ChartValues<ConvergenceValues> ChartValues { get; set; }
         private int LoadStepNumber { get; set; }
+        Viewport3D neo;
 
+
+
+
+        //public Viewport3D MainViewport = new Viewport3D();
+        public ModelVisual3D finalModel = new ModelVisual3D();
+        //public Dictionary<int, INode> nodes = new Dictionary<int, INode>();
+        //public Dictionary<int, Dictionary<int, int>> elementsConnectivity = new Dictionary<int, Dictionary<int, int>>();
+        // The main object model group.
+        private Model3DGroup MainModel3Dgroup = new Model3DGroup();
+
+        // The camera.
+        private PerspectiveCamera TheCamera;
+
+        // The camera's current location.
+        private double CameraPhi = 0; //Math.PI / 6.0;       // 30 degrees
+        private double CameraTheta = 0;// Math.PI / 6.0;     // 30 degrees
+#if SURFACE2
+        private double CameraR = 3.0;
+#else
+        private double CameraR = 300.0;
+#endif
+
+        // The change in CameraPhi when you press the up and down arrows.
+        private const double CameraDPhi = 0.1;
+
+        // The change in CameraTheta when you press the left and right arrows.
+        private const double CameraDTheta = 0.1;
+
+        // The change in CameraR when you press + or -.
+        private const double CameraDR = 0.1 * 100;
 
 
         public MainWindow()
@@ -47,8 +78,214 @@ namespace GFEC
             LoadComboBox();
             gnuplotImage.Source = null;
             ConvergenceResults();
+            Window_Loaded();
+            this.KeyDown += Window_KeyDown;
+
             
         }
+
+
+
+
+
+
+
+
+
+
+
+
+        private void Window_Loaded()
+        {
+            // Give the camera its initial position.
+            TheCamera = new PerspectiveCamera();
+            TheCamera.FieldOfView = 60;
+            ViewportGraphics.Camera = TheCamera;
+            PositionCamera();
+
+            // Define lights.
+            DefineLights();
+
+            // Create the model.
+            DefineModel(MainModel3Dgroup);
+
+            // Add the group of models to a ModelVisual3D.
+            ModelVisual3D model_visual = new ModelVisual3D();
+            model_visual.Content = MainModel3Dgroup;
+
+            // Add the main visual to the viewportt.
+            ViewportGraphics.Children.Add(model_visual);
+        }
+
+        //Define the lights.
+        private void DefineLights()
+        {
+            AmbientLight ambient_light = new AmbientLight(Colors.Gray);
+            DirectionalLight directional_light =
+                new DirectionalLight(Colors.Gray, new Vector3D(-1.0, -3.0, -2.0));
+            MainModel3Dgroup.Children.Add(ambient_light);
+            MainModel3Dgroup.Children.Add(directional_light);
+        }
+
+        // Add the model to the Model3DGroup.
+        private void DefineModel(Model3DGroup model_group)
+        {
+            // Make a mesh to hold the surface.
+            MeshGeometry3D mesh = new MeshGeometry3D();
+
+            // Make the surface's points and triangles.
+#if SURFACE2
+            const double xmin = -1.5;
+            const double xmax = 1.5;
+            const double dx = 0.05;
+            const double zmin = -1.5;
+            const double zmax = 1.5;
+            const double dz = 0.05;
+#else
+            const double xmin = -5;
+            const double xmax = 5;
+            const double dx = 0.5;
+            const double zmin = -5;
+            const double zmax = 5;
+            const double dz = 0.5;
+#endif
+            for (double x = xmin; x <= xmax - dx; x += dx)
+            {
+                for (double z = zmin; z <= zmax - dz; z += dx)
+                {
+                    // Make points at the corners of the surface
+                    // over (x, z) - (x + dx, z + dz).
+                    Point3D p00 = new Point3D(x, F(x, z), z);
+                    Point3D p10 = new Point3D(x + dx, F(x + dx, z), z);
+                    Point3D p01 = new Point3D(x, F(x, z + dz), z + dz);
+                    Point3D p11 = new Point3D(x + dx, F(x + dx, z + dz), z + dz);
+
+                    // Add the triangles.
+                    AddTriangle(mesh, p00, p01, p11);
+                    AddTriangle(mesh, p00, p11, p10);
+                }
+            }
+            Console.WriteLine(mesh.Positions.Count + " points");
+            Console.WriteLine(mesh.TriangleIndices.Count / 3 + " triangles");
+
+            // Make the surface's material using a solid orange brush.
+            DiffuseMaterial surface_material = new DiffuseMaterial(Brushes.Orange);
+
+            // Make the mesh's model.
+            GeometryModel3D surface_model = new GeometryModel3D(mesh, surface_material);
+
+            // Make the surface visible from both sides.
+            surface_model.BackMaterial = surface_material;
+
+            // Add the model to the model groups.
+            model_group.Children.Add(surface_model);
+        }
+
+        // The function that defines the surface we are drawing.
+        private double F(double x, double z)
+        {
+#if SURFACE2
+            const double two_pi = 2 * 3.14159265;
+            double r2 = x * x + z * z;
+            double r = Math.Sqrt(r2);
+            double theta = Math.Atan2(z, x);
+            return Math.Exp(-r2) * Math.Sin(two_pi * r) * Math.Cos(3 * theta);
+#else
+            double r2 = x * x + z * z;
+            return 8 * Math.Cos(r2 / 2) / (2 + r2);
+#endif
+        }
+
+        // Add a triangle to the indicated mesh.
+        private void AddTriangle(MeshGeometry3D mesh, Point3D point1, Point3D point2, Point3D point3)
+        {
+            // Get the points' indices.
+            int index1 = AddPoint(mesh.Positions, point1);
+            int index2 = AddPoint(mesh.Positions, point2);
+            int index3 = AddPoint(mesh.Positions, point3);
+
+            // Create the triangle.
+            mesh.TriangleIndices.Add(index1);
+            mesh.TriangleIndices.Add(index2);
+            mesh.TriangleIndices.Add(index3);
+        }
+
+        // Create the point and return its new index.
+        private int AddPoint(Point3DCollection points, Point3D point)
+        {
+            // Create the point and return its index.
+            points.Add(point);
+            return points.Count - 1;
+        }
+
+        // Adjust the camera's position.
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Up:
+                    CameraPhi += CameraDPhi;
+                    if (CameraPhi > Math.PI / 2.0) CameraPhi = Math.PI / 2.0;
+                    break;
+                case Key.Down:
+                    CameraPhi -= CameraDPhi;
+                    if (CameraPhi < -Math.PI / 2.0) CameraPhi = -Math.PI / 2.0;
+                    break;
+                case Key.Left:
+                    CameraTheta += CameraDTheta;
+                    break;
+                case Key.Right:
+                    CameraTheta -= CameraDTheta;
+                    break;
+                case Key.Add:
+                case Key.OemPlus:
+                    CameraR -= CameraDR;
+                    if (CameraR < CameraDR) CameraR = CameraDR;
+                    break;
+                case Key.Subtract:
+                case Key.OemMinus:
+                    CameraR += CameraDR;
+                    break;
+            }
+
+            // Update the camera's position.
+            PositionCamera();
+        }
+
+        // Position the camera.
+        private void PositionCamera()
+        {
+            // Calculate the camera's position in Cartesian coordinates.
+            double y = CameraR * Math.Sin(CameraPhi);
+            double hyp = CameraR * Math.Cos(CameraPhi);
+            double x = hyp * Math.Cos(CameraTheta);
+            double z = hyp * Math.Sin(CameraTheta);
+            TheCamera.Position = new Point3D(x, y, z);
+
+            // Look toward the origin.
+            TheCamera.LookDirection = new Vector3D(-x, -y, -z);
+
+            // Set the Up direction.
+            TheCamera.UpDirection = new Vector3D(0, 1, 0);
+
+            // Console.WriteLine("Camera.Position: (" + x + ", " + y + ", " + z + ")");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         private void RunButton(object sender, RoutedEventArgs args)
         {
@@ -706,14 +943,28 @@ namespace GFEC
             plotMesh.elementsConnectivity = elementsConnectivity;
             plotMesh.nodes = nodes;
             plotMesh.Window_Loaded();
+
+
             //ViewportGraphics = plotMesh.MainViewport;
-            //ViewportGraphics.UpdateLayout();
-            Window secondWindow = new Window();
-            secondWindow.Show();
-            secondWindow.KeyDown += plotMesh.Window_KeyDown;
-            secondWindow.Content = plotMesh.MainViewport;
+            neo = plotMesh.MainViewport;
+            //Window secondWindow = new Window();
+            //secondWindow.Show();
+            //secondWindow.KeyDown += plotMesh.Window_KeyDown;
+            //secondWindow.Content = plotMesh.MainViewport;
             //ViewportGraphics.InvalidateVisual();
-            //ViewportGraphics.Children.Add(plotMesh.finalModel);
+
+            viewport3DGrid.Children.Remove(ViewportGraphics);
+            ViewportGraphics = plotMesh.MainViewport;
+            viewport3DGrid.Children.Add(ViewportGraphics);
+
+
+            CompositionTarget.Rendering += TestMethod;
+        }
+       
+        private void TestMethod(object sender, EventArgs e)
+        {
+            ViewportGraphics = neo;
+            ViewportGraphics.UpdateLayout();
         }
     }
 
