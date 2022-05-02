e94e7ddd2521455ce0104abc679a7a9c93fc7a9a
Vasilis Merevis <vasilis.merevis@outlook.com>
2022-05-02 23:38:46 +0300

    Addition of BumperExample

---
+++ GFEC/Examples/BumperExample.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace GFEC
+{
+    public static class BumperExample
+    {
+        public static ISolver structuralSolution;
+        public static  Dictionary<int, INode> nodes;
+        public static Dictionary<int, Dictionary<int, int>> elementsConnectivity;
+
+
+
+
+        private static void CreateStructuralBoundaryConditions()
+        {
+            throw new Exception("Not implemented");
+        }
+
+        private static void CreateStructuralLoadVector()
+        {
+            throw new Exception("Not implemented");
+        }
+
+       
+        private static Dictionary<int, bool[]> CreateNodeFAT()
+        {
+            int totalNodes = nodes.Count;
+            Dictionary<int, bool[]> nodeFAT = new Dictionary<int, bool[]>();
+            for (int i = 1; i <= totalNodes; i++)
+            {
+                nodeFAT[i] = new bool[] { true, true, true, true, true, true };
+            }
+            return nodeFAT;
+        }
+
+        private static Dictionary<int, IElementProperties> CreateElementProperties()
+        {
+            double E = 200e9;
+            string type = "Shell2DQuadratic4";
+            Dictionary<int, IElementProperties> elementProperties = new Dictionary<int, IElementProperties>();
+            int totalElements = elementsConnectivity.Count;
+            for (int i = 1; i <= totalElements; i++)
+            {
+                elementProperties[i] = new ElementProperties(E, type);
+            }
+
+            for (int i = 1; i <= totalElements; i++)
+            {
+                elementProperties[i].Density = 100;
+                elementProperties[i].Thickness = 0.005;
+            }
+            return elementProperties;
+        }
+        
+
+        private static IAssembly CreateAssembly()
+        {
+            IAssembly assembly = new Assembly();
+            assembly.ElementsProperties = CreateElementProperties();
+            assembly.NodeFreedomAllocationList = CreateNodeFAT();
+            CreateStructuralBoundaryConditions();
+            CreateStructuralLoadVector();
+            assembly.BoundedDOFsVector = structuralBoundaryConditions;
+            return assembly;
+        }
+
+       
+        public static Results RunStaticExample()
+        {
+            #region Structural
+            IAssembly elementsAssembly = CreateAssembly();
+            elementsAssembly.CreateElementsAssembly();
+            elementsAssembly.ActivateBoundaryConditions = true;
+            double[,] globalStiffnessMatrix = elementsAssembly.CreateTotalStiffnessMatrix();
+            structuralSolution.LinearScheme = new LUFactorization();
+            structuralSolution.NonLinearScheme.Tolerance = 1e-4;
+            structuralSolution.ActivateNonLinearSolver = true;
+            structuralSolution.NonLinearScheme.numberOfLoadSteps = 40;
+
+            double[] reducedExternalForces3 = BoundaryConditionsImposition.ReducedVector(externalForces3, elementsAssembly.BoundedDOFsVector);
+            structuralSolution.AssemblyData = elementsAssembly;
+            structuralSolution.Solve(reducedExternalForces3);
+            double[] solvector3 = structuralSolution.GetSolution();
+            elementsAssembly.UpdateDisplacements(solvector3);
+            ShowToGUI.PlotFinalGeometry(elementsAssembly);
+            double[] fullSolVector3 = BoundaryConditionsImposition.CreateFullVectorFromReducedVector(solvector3, elementsAssembly.BoundedDOFsVector);
+            Dictionary<int, INode> finalNodes = Assembly.CalculateFinalNodalCoordinates(elementsAssembly.Nodes, fullSolVector3);
+            double[] xFinalNodalCoor = Assembly.NodalCoordinatesToVectors(finalNodes).Item1;
+            double[] yFinalNodalCoor = Assembly.NodalCoordinatesToVectors(finalNodes).Item2;
+            Dictionary<int, double[]> allStepsSolutions = structuralSolution.GetAllStepsSolutions();
+
+            
+            List<double[]> structuralSolutions = new List<double[]>();
+
+            #endregion
+
+            return new Results() { NonlinearSolution = structuralSolutions, SelectedDOF = 2, SolutionType = "Nonlinear" };
+        }
+
+        public static void RunDynamicExample()
+        {
+            IAssembly elementsAssembly = CreateAssembly();
+            elementsAssembly.CreateElementsAssembly();
+            elementsAssembly.ActivateBoundaryConditions = true;
+
+            InitialConditions initialValues = new InitialConditions();
+            initialValues.InitialAccelerationVector = new double[6];
+            initialValues.InitialDisplacementVector = new double[6];
+            //initialValues.InitialDisplacementVector[7] = -0.02146;
+            initialValues.InitialVelocityVector = new double[6];
+            initialValues.InitialTime = 0.0;
+
+            ExplicitSolver newSolver = new ExplicitSolver(1.0, 10000);
+            newSolver.Assembler = elementsAssembly;
+
+            newSolver.InitialValues = initialValues;
+            newSolver.ExternalForcesVector = new double[] { 0, 0, 0, 0, -50000, -50000 };
+            newSolver.LinearSolver = new CholeskyFactorization();
+            newSolver.ActivateNonLinearSolution = true;
+            newSolver.SolveNewmark();
+            newSolver.PrintExplicitSolution();//
+        }
+
+    }
+}
+
